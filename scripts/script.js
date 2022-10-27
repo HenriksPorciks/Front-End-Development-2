7e5820a9b392f7134f2e3dcc89fd0f2043e60e2c
Henriks Porciks <henriksporciks@gmail.com>
2022-10-27 14:23:24 +0200

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,22 @@
-// JavaScript Document
+/* menu openen */
+
+var openButton = document.querySelector("header > button");
+
+openButton.addEventListener("click", menuOpenen);
+
+function menuOpenen() {
+  var deNav = document.querySelector("header > nav");
+
+  deNav.classList.add("open");
+}
+
+/* menu sluiten */
+var sluitButton = document.querySelector("header nav button");
+
+sluitButton.addEventListener("click", menuSluiten);
+
+function menuSluiten() {
+  var deNav = document.querySelector("header > nav");
+
+  deNav.classList.remove("open");
+}
