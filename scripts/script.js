1bb920c59ac2e087d98abd2d3a70e773a85a6f77
Henriks Porciks <henriksporciks@gmail.com>
2022-11-07 17:12:33 +0100

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -20,3 +20,14 @@ function menuSluiten() {
 
   deNav.classList.remove("open");
 }
+
+// FLOEP FLAP van SANNE
+// eerste uitklapmenu met forum hulp nog niet eruit gekomen
+var deEersteButton = document.querySelector("section:nth-of-type(3) button");
+
+deEersteButton.addEventListener("click", toggleEersteMenu);
+
+function toggleEersteMenu() {
+  var hetEersteMenu = document.querySelector("section:nth-of-type(3)");
+  hetEersteMenu.classList.toggle("open");
+}
